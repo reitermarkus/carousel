19f3a07b20df5e105008130b58af7e9c696acb39
Markus Reiter <me@reitermark.us>
2017-04-07 14:50:18 +0200

    Split cube indices into groups (for each side).

--- cube.c
+++ cube.c
@@ -33,10 +33,52 @@ void cube(double h, struct vertex** vertices, long* vertices_size, GLushort** in
   SET_VERTEX_POSITION((*vertices)[7], -h, h, -h);
   SET_VERTEX_COLOR((*vertices)[7], R(RGB_RAND), G(RGB_RAND), B(RGB_RAND));
 
-  (*indices)[0] = 0; (*indices)[1] = 1; (*indices)[2] = 2; (*indices)[3] = 2; (*indices)[4] = 3; (*indices)[5] = 0;
-  (*indices)[6] = 1; (*indices)[7] = 5; (*indices)[8] = 6; (*indices)[9] = 6; (*indices)[10] = 2; (*indices)[11] = 1;
-  (*indices)[12] = 7; (*indices)[13] = 6; (*indices)[14] = 5; (*indices)[15] = 5; (*indices)[16] = 4; (*indices)[17] = 7;
-  (*indices)[18] = 4; (*indices)[19] = 0; (*indices)[20] = 3; (*indices)[21] = 3; (*indices)[22] = 7; (*indices)[23] = 4;
-  (*indices)[24] = 4; (*indices)[25] = 5; (*indices)[26] = 1; (*indices)[27] = 1; (*indices)[28] = 0; (*indices)[29] = 4;
-  (*indices)[30] = 3; (*indices)[31] = 2; (*indices)[32] = 6; (*indices)[33] = 6; (*indices)[34] = 7; (*indices)[35] = 3;
+  // Front side.
+  (*indices)[0] = 0;
+  (*indices)[1] = 1;
+  (*indices)[2] = 2;
+  (*indices)[3] = 2;
+  (*indices)[4] = 3;
+  (*indices)[5] = 0;
+
+  // Right side.
+  (*indices)[6]  = 1;
+  (*indices)[7]  = 5;
+  (*indices)[8]  = 6;
+  (*indices)[9]  = 6;
+  (*indices)[10] = 2;
+  (*indices)[11] = 1;
+
+  // Backside.
+  (*indices)[12] = 7;
+  (*indices)[13] = 6;
+  (*indices)[14] = 5;
+  (*indices)[15] = 5;
+  (*indices)[16] = 4;
+  (*indices)[17] = 7;
+
+  // Left side.
+  (*indices)[18] = 4;
+  (*indices)[19] = 0;
+  (*indices)[20] = 3;
+  (*indices)[21] = 3;
+  (*indices)[22] = 7;
+  (*indices)[23] = 4;
+
+  // Bottom side.
+  (*indices)[24] = 4;
+  (*indices)[25] = 5;
+  (*indices)[26] = 1;
+  (*indices)[27] = 1;
+  (*indices)[28] = 0;
+  (*indices)[29] = 4;
+
+  // Top side.
+  (*indices)[30] = 3;
+  (*indices)[31] = 2;
+  (*indices)[32] = 6;
+  (*indices)[33] = 6;
+  (*indices)[34] = 7;
+  (*indices)[35] = 3;
+
 }
