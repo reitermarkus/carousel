e3b61ec23517b0ff1e52b42d8e0596665d23f931
Markus Reiter <me@reitermark.us>
2017-05-05 19:30:35 +0200

    Normalize vertex normals.

--- helper/vertex.c
+++ helper/vertex.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 static struct normal face_normal(struct position p1, struct position p2, struct position p3) {
   struct position u = {
@@ -39,26 +40,31 @@ static void calculate_face_normals(struct object_data* obj) {
   }
 }
 
+static struct normal* normalize(struct normal* normal) {
+  float factor = sqrt(pow(normal->a, 2) + pow(normal->b, 2) + pow(normal->c, 2));
+
+  normal->a /= factor;
+  normal->b /= factor;
+  normal->c /= factor;
+
+  return normal;
+}
+
 static void calculate_vertex_normals(struct object_data* obj) {
   puts("Calculating vertex normals ...");
 
   for(int i = 0; i < obj->vertex_count; i++) {
-    float sum[3] = {0, 0, 0};
-    int count = 0;
+    struct normal normal_sum = {0, 0, 0};
 
     for(int j = 0; j < obj->index_count; j++) {
       if ((obj->indices[j].a == i) || (obj->indices[j].b == i) || (obj->indices[j].c == i)) {
-        sum[0] += obj->face_normals[j].a;
-        sum[1] += obj->face_normals[j].b;
-        sum[2] += obj->face_normals[j].c;
-
-        count++;
+        normal_sum.a += obj->face_normals[j].a;
+        normal_sum.b += obj->face_normals[j].b;
+        normal_sum.c += obj->face_normals[j].c;
       }
     }
 
-    obj->vertices[i].normal.a = sum[0] / (float)count;
-    obj->vertices[i].normal.b = sum[1] / (float)count;
-    obj->vertices[i].normal.c = sum[2] / (float)count;
+    obj->vertices[i].normal = *normalize(&normal_sum);
 
     printf("%f\t%f\t%f\n", obj->vertices[i].normal.a, obj->vertices[i].normal.b, obj->vertices[i].normal.c);
   }
