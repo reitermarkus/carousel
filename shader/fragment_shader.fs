b5c4e358120def573e3b5eaad8d77fe3165d6f0f
Markus Reiter <me@reitermark.us>
2017-05-10 10:53:53 +0200

    Use view matrix in fragment shader.

--- shader/fragment_shader.fs
+++ shader/fragment_shader.fs
@@ -4,9 +4,8 @@ in vec3 v_position;
 in vec4 v_color;
 in vec3 v_normal;
 in vec2 v_texture;
+in vec3 light_pos_1;
 
-uniform vec3 light_position_1;
-uniform vec3 light_position_2;
 uniform vec3 light_color_1;
 uniform vec3 light_color_2;
 uniform float diffuse_factor;
@@ -22,14 +21,14 @@ void main() {
   vec3 diffuse_sum = vec3(0);
   vec3 spec_sum = vec3(0);
 
-  vec3 n = normalize(v_normal);
-  vec3 s = normalize(light_position_1 - v_position);
-  vec3 v = normalize(-v_position);
-  vec3 r = reflect(-s, n);
+  vec3 normal = normalize(v_normal);
+  vec3 light_direction = normalize(light_pos_1 - v_position);
+  vec3 view_direction = normalize(-v_position);
+  vec3 reflect_direction = reflect(-light_direction, normal);
 
   vec3 ambient = vec3(ambient_factor);
-  vec3 diffuse = vec3(diffuse_factor) * max(dot(s, n), 0.0);
-  vec3 specular = vec3(specular_factor) * max(dot(r, v), 0.0);
+  vec3 diffuse = vec3(diffuse_factor) * max(dot(normal, light_direction), 0.0);
+  vec3 specular = vec3(specular_factor) * pow(max(dot(reflect_direction, view_direction), 0.0), 100);
 
   ambient_sum += ambient;
   diffuse_sum += diffuse;
@@ -39,5 +38,5 @@ void main() {
 
   vec4 texture_color = texture(tiger_texture, v_texture);
 
-  fragment_color = v_color * vec4(ambient_sum, 1) /* * texture_color */ + vec4((diffuse_sum + spec_sum) * light_color_1, 0);
+  fragment_color = v_color * vec4(ambient_sum + ((diffuse_sum + spec_sum) * light_color_1), 1) /* * texture_color */;
 }
--- shader/vertex_shader.vs
+++ shader/vertex_shader.vs
@@ -14,11 +14,16 @@ out vec4 v_color;
 out vec2 v_texture;
 out vec3 v_normal;
 
+uniform vec3 light_position_1;
+out vec3 light_pos_1;
+
 void main() {
   gl_Position = projection_matrix * view_matrix * model_matrix * vec4(position.x, position.y, position.z, 1.0);
 
-  v_position = vec3(model_matrix * vec4(position, 1));
+  v_position = vec3(view_matrix * model_matrix * vec4(position, 1));
   v_color = color;
-  v_normal = normal;
+  v_normal = mat3(transpose(inverse(view_matrix * model_matrix))) * normal;
   v_texture = texture;
+
+  light_pos_1 = vec3(view_matrix * vec4(light_position_1, 1.0));
 }
