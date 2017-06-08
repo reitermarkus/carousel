bd8f39d8551af301acb445e7f6fe04435afe2336
Markus Reiter <me@reitermark.us>
2017-06-08 12:46:59 +0200

    Fix texture parsing.

--- carousel.c
+++ carousel.c
@@ -248,40 +248,53 @@ void init_ext_obj(struct object_data* obj, char* filename){
 
   // Copy mesh data from structs into appropriate arrays.
 
-  obj->vertex_count = ext_obj.vertex_count;
+  obj->vertex_count = ext_obj.vertex_texture_count;
   obj->index_count = ext_obj.face_count;
-  obj->texture_count = ext_obj.face_count * 3;
+  obj->texture_count = ext_obj.vertex_texture_count;
 
-  obj->vertices = malloc(obj->vertex_count * sizeof(*obj->vertices));
+  obj->vertices = malloc(obj->texture_count * sizeof(*obj->vertices));
   obj->indices = malloc(obj->index_count * sizeof(*obj->indices));
   obj->textures = malloc(obj->texture_count * sizeof(*obj->textures));
 
-  // Vertices
-  for (int i = 0; i < ext_obj.vertex_count; i++) {
-    SET_VERTEX_POSITION(obj->vertices[i],
-      ext_obj.vertex_list[i]->e[0],
-      ext_obj.vertex_list[i]->e[1],
-      ext_obj.vertex_list[i]->e[2]
-    );
-
-    SET_VERTEX_COLOR(obj->vertices[i], R(RGB_RAND), G(RGB_RAND), B(RGB_RAND), ALPHA_RAND);
+  for (size_t i = 0; i < obj->vertex_count; i++) {
+    SET_VERTEX_COLOR(obj->vertices[i], 1.0, 1.0, 1.0, 1.0);
   }
 
   for (int i = 0; i < ext_obj.face_count; i++) {
+    // Vertices
+    int index0 = ext_obj.face_list[i]->vertex_index[0];
+    int index1 = ext_obj.face_list[i]->vertex_index[1];
+    int index2 = ext_obj.face_list[i]->vertex_index[2];
+
     // Indices
-    obj->indices[i].a = ext_obj.face_list[i]->vertex_index[0];
-    obj->indices[i].b = ext_obj.face_list[i]->vertex_index[1];
-    obj->indices[i].c = ext_obj.face_list[i]->vertex_index[2];
+    obj->indices[i].a = ext_obj.face_list[i]->texture_index[0];
+    obj->indices[i].b = ext_obj.face_list[i]->texture_index[1];
+    obj->indices[i].c = ext_obj.face_list[i]->texture_index[2];
 
-    // Textures
-    for (int j = 0; j < 3; j++) {
-      int index = ext_obj.face_list[i]->texture_index[j];
+    SET_VERTEX_POSITION(obj->vertices[obj->indices[i].a],
+      ext_obj.vertex_list[index0]->e[0],
+      ext_obj.vertex_list[index0]->e[1],
+      ext_obj.vertex_list[index0]->e[2]
+    );
 
-      if (index >= 0) {
-        obj->textures[index].u = ext_obj.vertex_texture_list[index]->e[0];
-        obj->textures[index].v = ext_obj.vertex_texture_list[index]->e[1];
-      }
-    }
+    SET_VERTEX_POSITION(obj->vertices[obj->indices[i].b],
+      ext_obj.vertex_list[index1]->e[0],
+      ext_obj.vertex_list[index1]->e[1],
+      ext_obj.vertex_list[index1]->e[2]
+    );
+
+    SET_VERTEX_POSITION(obj->vertices[obj->indices[i].c],
+      ext_obj.vertex_list[index2]->e[0],
+      ext_obj.vertex_list[index2]->e[1],
+      ext_obj.vertex_list[index2]->e[2]
+    );
+
+    obj->textures[obj->indices[i].a].u = ext_obj.vertex_texture_list[obj->indices[i].a]->e[0];
+    obj->textures[obj->indices[i].a].v = ext_obj.vertex_texture_list[obj->indices[i].a]->e[1];
+    obj->textures[obj->indices[i].b].u = ext_obj.vertex_texture_list[obj->indices[i].b]->e[0];
+    obj->textures[obj->indices[i].b].v = ext_obj.vertex_texture_list[obj->indices[i].b]->e[1];
+    obj->textures[obj->indices[i].c].u = ext_obj.vertex_texture_list[obj->indices[i].c]->e[0];
+    obj->textures[obj->indices[i].c].v = ext_obj.vertex_texture_list[obj->indices[i].c]->e[1];
   }
 
   delete_obj_scene_data(&ext_obj);
